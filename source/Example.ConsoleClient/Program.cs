e4421408dd843632608b51d06b1c4ea757413c45
Ayaz Duru <ayzdru@outlook.com>
2020-01-15 11:30:05 +0300

    Simple.OData.Client package added in ConsoleClient project.

--- source/Example.ConsoleClient/Program.cs
+++ source/Example.ConsoleClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using IdentityModel.Client;
 using Newtonsoft.Json.Linq;
+using Simple.OData.Client;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -10,6 +12,8 @@ namespace Example.ConsoleClient
     {
         static async Task Main(string[] args)
         {
+           
+
             // discover endpoints from metadata
             var client = new HttpClient();
 
@@ -43,6 +47,16 @@ namespace Example.ConsoleClient
             var apiClient = new HttpClient();
             apiClient.BaseAddress = new Uri("http://localhost:5001");
             apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+
+            var odataClient = new ODataClient(new ODataClientSettings(apiClient, new Uri("api", UriKind.Relative))
+            {
+                IgnoreResourceNotFoundException = true,
+                OnTrace = (x, y) => Console.WriteLine(string.Format(x, y))
+
+            });
+            var orders = await odataClient.For("Orders").Select("Customer").QueryOptions(new Dictionary<string, object> { { "api-version", 3.0 } }).FindEntriesAsync();
+
 
             var response = await apiClient.GetAsync("/api/Orders?api-version=3.0&$count=true");
             if (!response.IsSuccessStatusCode)
